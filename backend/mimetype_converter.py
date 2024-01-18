85d8838ffdbcce8af2f9e70b8ebb0457f064dd43
Mahad Asif <mahad.asif.git@gmail.com>
2024-01-18 23:00:16 +0500

    refactor

--- backend/mimetype_converter.py
+++ backend/mimetype_converter.py
@@ -12,26 +12,24 @@ class MimetypeConverter:
         image = Image.open(self.file.name)
         output_file_path = self.generate_output_path()
 
-        print(f"*************************{output_file_path}**********************")
-
-        if self.source_mimetype == 'image/png' and self.target_mimetype == 'image/jpeg':
+        if self.png_to_jpeg():
             image = image.convert('RGB')
             image.save(output_file_path, 'JPEG', quality=95)
-        elif (self.source_mimetype == 'image/jpeg' or self.source_mimetype == 'image/png') and self.target_mimetype == 'image/bmp':
+        elif self.jpeg_to_bmp() or self.png_to_bmp():
             image = image.convert(mode="P", palette=Image.ADAPTIVE, colors=256)
             image = image.convert("RGB")
             image.save(output_file_path)
-        elif self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/png':
+        elif self.jpeg_to_png():
             image.save(output_file_path, 'PNG')
-        elif (self.source_mimetype == 'image/jpeg' or self.source_mimetype == 'image/png') and self.target_mimetype == 'image/webp':
+        elif self.jpeg_to_webp() or self.png_to_webp():
             image.save(output_file_path, 'WEBP')
-        elif self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/png':
+        elif self.webp_to_png():
             image = image.convert("RGB")
             image.save(output_file_path, 'PNG')
-        elif self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/jpeg':
+        elif self.webp_to_jpeg():
             image = image.convert("RGB")
             image.save(output_file_path, 'JPEG')
-        elif self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/bmp':
+        elif self.webp_to_bmp():
             image = image.convert("RGB")
             image.save(output_file_path, 'BMP')
 
@@ -47,14 +45,35 @@ class MimetypeConverter:
         # Construct the output file path in the same directory with a different extension
         return os.path.join(file_directory, file_name.split('.')[0] + '.' + self.target_mimetype.split('/')[-1]).replace('uploaded', 'converted')
 
+    def png_to_jpeg(self):
+        self.source_mimetype == 'image/png' and self.target_mimetype == 'image/jpeg'
+
+    def png_to_bmp(self):
+        self.source_mimetype == 'image/png' and self.target_mimetype == 'image/bmp'
+
+    def png_to_webp(self):
+        self.source_mimetype == 'image/png' and self.target_mimetype == 'image/webp'
+
+    def jpeg_to_bmp(self):
+        self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/bmp'
+
+    def jpeg_to_png(self):
+        self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/png'
+
+    def jpeg_to_webp(self):
+        self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/webp'
+
+    def webp_to_png(self):
+        self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/png'
+
+    def webp_to_jpeg(self):
+        self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/jpeg'
+
+    def webp_to_bmp(self):
+        self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/bmp'
+
+
 
     # def generate_output_path(self):
         # return f"{self.file.path.split('.')[0]}.{self.target_mimetype.split('/')[-1]}".replace('uploaded', 'converted')
 
-# Example usage:
-# source_mimetype = 'image/png'
-# target_mimetype = 'image/jpeg'
-# file = YourFileObjectHere  # Replace with your actual file object
-
-# converter = ImageConverter(source_mimetype, target_mimetype, file)
-# output_path = converter.convert()
