360a3c53dad1bb79222a1625296457161b9fb3d5
Mahad Asif <mahad.asif.git@gmail.com>
2024-01-24 22:48:55 +0500

    added new conversions

--- backend/mimetype_converter.py
+++ backend/mimetype_converter.py
@@ -3,6 +3,9 @@ from reportlab.pdfgen import canvas
 from pdf2docx import Converter
 import fitz  # PyMuPDF
 from docx import Document
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import subprocess
 import os
 import pdb
 
@@ -42,6 +45,15 @@ class MimetypeConverter:
         elif self.is_webp_to_bmp():
             image = image.convert("RGB")
             image.save(output_file_path, 'BMP')
+        elif self.is_gif_to_png():
+            image.save(output_file_path, 'PNG')
+        elif self.is_gif_to_jpeg():
+            image = image.convert("RGB")
+            image.save(output_file_path, 'JPEG')
+        elif self.is_gif_to_webp():
+            image.save(output_file_path, 'WEBP')
+        elif self.is_gif_to_bmp():
+            image.save(output_file_path, 'BMP')
         elif self.is_png_to_pdf():
             self.convert_image_to_pdf(image, output_file_path)
         elif self.is_jpeg_to_pdf():
@@ -49,15 +61,22 @@ class MimetypeConverter:
         elif self.is_webp_to_pdf():
             self.convert_image_to_pdf(image, output_file_path)
 
-
         return output_file_path
 
     def convert_file(self):
         if self.is_pdf_to_docx():
             output_file_path = self.generate_output_file_path('docx')
             self.convert_pdf_to_docx(self.file.name, output_file_path)
+            return output_file_path
+        elif self.is_pdf_to_doc():
+            output_file_path = self.generate_output_file_path('docx')
+            self.convert_pdf_to_doc(self.file.name, output_file_path)
+            return output_file_path
+        elif self.is_docx_to_pdf():
+            output_file_path = self.generate_output_file_path('pdf')
+            self.convert_docx_to_pdf(self.file.name, output_file_path)
+            return output_file_path
 
-        return output_file_path
 
     def generate_output_file_path(self, target_extension=None):
         # Get the directory part of the original file path
@@ -79,6 +98,9 @@ class MimetypeConverter:
     def is_png_to_webp(self):
         return self.source_mimetype == 'image/png' and self.target_mimetype == 'image/webp'
 
+    def is_png_to_pdf(self):
+        return self.source_mimetype == 'image/png' and self.target_mimetype == 'application/pdf'
+
     def is_jpeg_to_bmp(self):
         return self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/bmp'
 
@@ -87,6 +109,9 @@ class MimetypeConverter:
 
     def is_jpeg_to_webp(self):
         return self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'image/webp'
+
+    def is_jpeg_to_pdf(self):
+        return self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'application/pdf'
 
     def is_webp_to_png(self):
         return self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/png'
@@ -97,17 +122,29 @@ class MimetypeConverter:
     def is_webp_to_bmp(self):
         return self.source_mimetype == 'image/webp' and self.target_mimetype == 'image/bmp'
 
-    def is_png_to_pdf(self):
-        return self.source_mimetype == 'image/png' and self.target_mimetype == 'application/pdf'
-
-    def is_jpeg_to_pdf(self):
-        return self.source_mimetype == 'image/jpeg' and self.target_mimetype == 'application/pdf'
-
     def is_webp_to_pdf(self):
         return self.source_mimetype == 'image/webp' and self.target_mimetype == 'application/pdf'
 
+    def is_gif_to_png(self):
+        return self.source_mimetype == 'image/gif' and self.target_mimetype == 'image/png'
+
+    def is_gif_to_jpeg(self):
+        return self.source_mimetype == 'image/gif' and self.target_mimetype == 'image/jpeg'
+
+    def is_gif_to_webp(self):
+        return self.source_mimetype == 'image/gif' and self.target_mimetype == 'image/webp'
+
+    def is_gif_to_bmp(self):
+        return self.source_mimetype == 'image/gif' and self.target_mimetype == 'image/bmp'
+
     def is_pdf_to_docx(self):
         return self.source_mimetype == 'application/pdf' and self.target_mimetype == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+    def is_pdf_to_doc(self):
+        return self.source_mimetype == 'application/pdf' and self.target_mimetype == 'application/msword'
+
+    def is_docx_to_pdf(self):
+        return self.source_mimetype == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' and self.target_mimetype == 'application/pdf'
 
     def convert_image_to_pdf(self, input_image, output_file_path):
         print('converting png image to pdf file ...')
@@ -119,6 +156,28 @@ class MimetypeConverter:
         cv = Converter(pdf_path)
         cv.convert(docx_path)
         cv.close()
+
+    def convert_pdf_to_doc(self, pdf_path, doc_path):
+        # Step 1: Extract text from PDF
+        pdf_document = fitz.open(pdf_path)
+        text = ""
+        for page_number in range(pdf_document.page_count):
+            page = pdf_document[page_number]
+            text += page.get_text()
+
+        # Step 2: Create DOC file using python-docx
+        doc = Document()
+        doc.add_paragraph(text)
+
+        # Save the DOC file
+        doc.save(doc_path)
+
+    def convert_docx_to_pdf(self, docx_path, pdf_path):
+        try:
+            subprocess.run(['unoconv', '-f', 'pdf', '-o', pdf_path, docx_path])
+            print(f"Conversion successful. PDF saved at: {pdf_path}")
+        except Exception as e:
+            print(f"Error converting file: {e}")
 
 
 
