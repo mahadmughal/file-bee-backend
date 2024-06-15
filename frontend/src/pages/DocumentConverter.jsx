5d60d8c5c8b1b4a214df1284f410c0fc4a7a3289
Mahad Asif <mahad.asif.git@gmail.com>
2024-06-15 12:29:48 +0300

    made converters dropdown functional

---
+++ frontend/src/pages/DocumentConverter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileConversion from '../components/FileConversion';
+
+function DocumentConverter() {
+  const fileType = "Document";
+
+  return (
+    <FileConversion fileType={fileType}/>
+  );
+}
+
+export default DocumentConverter;
---
+++ frontend/src/pages/ImageConverter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileConversion from '../components/FileConversion';
+
+function ImageConverter() {
+  const fileType = 'Image'
+
+  return (
+    <FileConversion  fileType={fileType} />
+  );
+}
+
+export default ImageConverter;
