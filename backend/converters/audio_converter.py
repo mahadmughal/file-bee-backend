3605c0eb4714cf5082586828cce24afe3d989be4
Mahad Asif <mahad.asif.git@gmail.com>
2024-09-23 18:57:31 +0300

    s3 changes in audio converter class

--- backend/converters/audio_converter.py
+++ backend/converters/audio_converter.py
@@ -1,5 +1,8 @@
 import os
 from pydub import AudioSegment
+from io import BytesIO
+from django.core.files.base import ContentFile
+from backend.utils.s3_utils import s3_client
 
 
 class AudioConverter:
@@ -9,120 +12,178 @@ class AudioConverter:
         self.file = file
 
     def convert(self):
+        input_audio = self.get_file_from_s3()
+
         if self.is_mp3_to_oga():
-            self.convert_mp3_to_oga()
+            output = self.convert_mp3_to_oga(input_audio)
         elif self.is_mp3_to_wav():
-            self.convert_mp3_to_wav()
+            output = self.convert_mp3_to_wav(input_audio)
         elif self.is_mp3_to_flac():
-            self.convert_mp3_to_flac()
+            output = self.convert_mp3_to_flac(input_audio)
         elif self.is_mp3_to_mp2():
-            self.convert_mp3_to_mp2()
+            output = self.convert_mp3_to_mp2(input_audio)
         elif self.is_wav_to_oga():
-            self.convert_wav_to_oga()
+            output = self.convert_wav_to_oga(input_audio)
         elif self.is_wav_to_mp3():
-            self.convert_wav_to_mp3()
+            output = self.convert_wav_to_mp3(input_audio)
         elif self.is_wav_to_flac():
-            self.convert_wav_to_flac()
+            output = self.convert_wav_to_flac(input_audio)
         elif self.is_wav_to_mp2():
-            self.convert_wav_to_mp2()
+            output = self.convert_wav_to_mp2(input_audio)
         elif self.is_flac_to_oga():
-            self.convert_flac_to_oga()
+            output = self.convert_flac_to_oga(input_audio)
         elif self.is_flac_to_mp3():
-            self.convert_flac_to_mp3()
+            output = self.convert_flac_to_mp3(input_audio)
         elif self.is_flac_to_wav():
-            self.convert_flac_to_wav()
+            output = self.convert_flac_to_wav(input_audio)
         elif self.is_flac_to_mp2():
-            self.convert_flac_to_mp2()
+            output = self.convert_flac_to_mp2(input_audio)
         elif self.is_mp2_to_oga():
-            self.convert_mp2_to_oga()
+            output = self.convert_mp2_to_oga(input_audio)
         elif self.is_mp2_to_mp3():
-            self.convert_mp2_to_mp3()
+            output = self.convert_mp2_to_mp3(input_audio)
         elif self.is_mp2_to_wav():
-            self.convert_mp2_to_wav()
+            output = self.convert_mp2_to_wav(input_audio)
         elif self.is_mp2_to_flac():
-            self.convert_mp2_to_flac()
+            output = self.convert_mp2_to_flac(input_audio)
 
-        return self.output_path
+        return ContentFile(output)
 
-    def convert_mp3_to_oga(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='oga')
+    def convert_mp3_to_oga(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp3')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='oga')
+        output_buffer.seek(0)
 
-    def convert_mp3_to_wav(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='wav')
+        return output_buffer.getvalue()
 
-    def convert_mp3_to_flac(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='flac')
+    def convert_mp3_to_wav(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp3')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='wav')
+        output_buffer.seek(0)
 
-    def convert_mp3_to_mp2(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='mp2')
+        return output_buffer.getvalue()
 
-    def convert_wav_to_oga(self):
-        audio = AudioSegment.from_file(self.file.path, format='wav')
-        audio.export(self.output_path(), format='oga')
+    def convert_mp3_to_flac(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp3')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='flac')
+        output_buffer.seek(0)
 
-    def convert_wav_to_mp3(self):
-        audio = AudioSegment.from_file(self.file.path, format='wav')
-        audio.export(self.output_path(), format='mp3')
+        return output_buffer.getvalue()
 
-    def convert_wav_to_flac(self):
-        audio = AudioSegment.from_file(self.file.path, format='wav')
-        audio.export(self.output_path(), format='flac')
+    def convert_mp3_to_mp2(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp3')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp2')
+        output_buffer.seek(0)
 
-    def convert_wav_to_mp2(self):
-        audio = AudioSegment.from_file(self.file.path, format='wav')
-        audio.export(self.output_path(), format='mp2')
+        return output_buffer.getvalue()
 
-    def convert_flac_to_oga(self):
-        audio = AudioSegment.from_file(self.file.path, format='flac')
-        audio.export(self.output_path(), format='oga')
+    def convert_wav_to_oga(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='wav')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='oga')
+        output_buffer.seek(0)
 
-    def convert_flac_to_mp3(self):
-        audio = AudioSegment.from_file(self.file.path, format='flac')
-        audio.export(self.output_path(), format='mp3')
+        return output_buffer.getvalue()
 
-    def convert_flac_to_wav(self):
-        audio = AudioSegment.from_file(self.file.path, format='flac')
-        audio.export(self.output_path(), format='wav')
+    def convert_wav_to_mp3(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='wav')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp3')
+        output_buffer.seek(0)
 
-    def convert_flac_to_mp2(self):
-        audio = AudioSegment.from_file(self.file.path, format='flac')
-        audio.export(self.output_path(), format='mp2')
+        return output_buffer.getvalue()
 
-    def convert_mp2_to_oga(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='oga')
+    def convert_wav_to_flac(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='wav')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='flac')
+        output_buffer.seek(0)
 
-    def convert_mp2_to_mp3(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='mp3')
+        return output_buffer.getvalue()
 
-    def convert_mp2_to_wav(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='wav')
+    def convert_wav_to_mp2(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='wav')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp2')
+        output_buffer.seek(0)
 
-    def convert_mp2_to_flac(self):
-        audio = AudioSegment.from_file(self.file.path, format='mp3')
-        audio.export(self.output_path(), format='flac')
+        return output_buffer.getvalue()
 
-    def output_path(self):
-        self.output_path = self.generate_output_file_path()
-        return self.output_path
+    def convert_flac_to_oga(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='flac')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='oga')
+        output_buffer.seek(0)
 
-    def generate_output_file_path(self, target_extension=None):
-        # Get the directory part of the original file path
-        file_directory = os.path.dirname(self.file.name)
+        return output_buffer.getvalue()
 
-        # Get the filename part of the original file path
-        file_name = os.path.basename(self.file.path)
-        target_extension = target_extension if target_extension else self.target_mimetype.split(
-            '/')[-1]
+    def convert_flac_to_mp3(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='flac')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp3')
+        output_buffer.seek(0)
 
-        # Construct the output file path in the same directory with a different extension
-        return (file_directory + '/' + file_name.split('.')[0] + '.' + target_extension).replace('uploaded', 'converted')
+        return output_buffer.getvalue()
+
+    def convert_flac_to_wav(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='flac')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='wav')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def convert_flac_to_mp2(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='flac')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp2')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def convert_mp2_to_oga(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp2')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='oga')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def convert_mp2_to_mp3(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp2')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='mp3')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def convert_mp2_to_wav(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp2')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='wav')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def convert_mp2_to_flac(self, input_audio):
+        audio = AudioSegment.from_file(input_audio, format='mp2')
+        output_buffer = BytesIO()
+        audio.export(output_buffer, format='flac')
+        output_buffer.seek(0)
+
+        return output_buffer.getvalue()
+
+    def get_file_from_s3(self):
+        file_content = s3_client.get_object(self.file.name)
+        file_object = BytesIO(file_content)
+        file_object.name = os.path.basename(os.path.basename(self.file.name))
+        file_object.seek(0)
+
+        return file_object
 
     def is_mp3_to_oga(self):
         return self.source_mimetype == 'audio/mpeg' and self.target_mimetype == 'audio/ogg'
